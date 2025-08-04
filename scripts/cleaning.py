2d2519229882799ba0ffd028728419cbaa3cf544
Katie <68813616+kmcd14@users.noreply.github.com>
2025-08-04 15:42:42 +0100

    script to clean, transform and format data in df columns

---
+++ scripts/cleaning.py
@@ -0,0 +1,80 @@
+# Clean data from the Irish Rail API
+from datetime import datetime
+import pandas as pd
+
+
+### Convert data types ###
+
+
+# convert object type date columns to datetime
+def object_to_date(df, columns):
+    """
+    Convert object type date columns to datetime.
+    """
+    for col in columns:
+        df[col] = pd.to_datetime(df[col], errors='coerce')
+    return df
+
+
+# convert object type columns to string
+def object_tostring(df, columns):
+    """
+    Convert object type columns to string.
+    """
+    for col in columns:
+        df[col] = df[col].astype(str)
+    return df
+
+
+# convert object type columns to float
+def object_to_float(df, columns):
+    """
+    Convert object type columns to float.
+    """
+    for col in columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    return df
+    
+
+# timestamp of fetch
+def fetch_timestamp():
+    """
+    Get the current timestamp and insert it into the DataFrame.
+    This function returns the current date and time in the format 'YYYY-MM-DD HH:MM:SS'.
+    """
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+
+##############################################
+
+
+### Cleaning functions ###
+
+# remove whitespace from string columns
+def remove_whitespace(df, columns):
+    """
+    Remove leading and trailing whitespace from string columns.
+    """
+    for col in columns:
+        df[col] = df[col].str.strip()
+    return df
+
+
+# remove rows with Null values in specified columns
+def remove_nulls(df, columns):
+    """
+    Remove rows with Null values in specified columns.
+    """
+    for col in columns:
+        df = df[df[col].notnull()]
+    return df
+
+# remove line breaks 
+def remove_linebreaks(df, columns):
+    """
+    Remove line breaks from string columns.
+    """
+    for col in columns:
+         df[col] = df[col].str.replace(r'\s*\n\s*', ' ', regex=True).str.strip()
+    return df
