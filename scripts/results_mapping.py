d34a70c31c0e9d5caa62bac619de22091d43bd3c
Katie <68813616+kmcd14@users.noreply.github.com>
2025-08-06 21:23:43 +0100

    changed the way columns where named to match api

--- scripts/results_mapping.py
+++ scripts/results_mapping.py
@@ -3,68 +3,69 @@
 ### Get current trains ###
 URL_CURRENT_TRAINS = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 FIELD_MAP_CURRENT_TRAINS = {
-    'train_code': 'TrainCode',
-    'direction': 'Direction',
-    'status': 'TrainStatus',
-    'date': 'TrainDate',
-    'message': 'PublicMessage',
-    'latitude': 'TrainLatitude',
-    'longitude': 'TrainLongitude'
+    'TrainCode': 'TrainCode',
+    'Direction': 'Direction',
+    'TrainStatus': 'TrainStatus',
+    'TrainDate': 'TrainDate',
+    'PublicMessage': 'PublicMessage',
+    'TrainLatitude': 'TrainLatitude',
+    'TrainLongitude': 'TrainLongitude',
+    'TrainType': 'TrainType'
 }
 
 ### Station information ###
-URL_STATION_INFO = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML "
+URL_STATION_INFO = "http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML"
 FIELD_MAP_STATION_INFO = {
-    'station_desc': 'StationDesc',
-    'station_code': 'StationCode',
-    'station_id': 'StationId',
-    'alias': 'StationAlias',
-    'latitude': 'StationLatitude',
-    'longitude': 'StationLongitude',
+    'StationDesc': 'StationDesc',
+    'StationCode': 'StationCode',
+    'StationId': 'StationId',
+    'StationAlias': 'StationAlias',
+    'StationLatitude': 'StationLatitude',
+    'StationLongitude': 'StationLongitude'
 }
 
 
 ### Train Movements ###
 URL_TRAIN_MOVEMENTS = "http://api.irishrail.ie/realtime/realtime.asmx/getTrainMovementsXML"
 FIELD_MAP_TRAIN_MOVEMENTS = {
-    'train_code': 'TrainCode',  
-    'train_date': 'TrainDate',
-    'location_code': 'LocationCode',
+    'TrainCode': 'TrainCode',
+    'TrainDate': 'TrainDate',
+    'LocationCode': 'LocationCode',
     'LocationFullName': 'LocationFullName',
     'LocationOrder': 'LocationOrder',
-    'LocationType': 'LocationType',  # O= Origin, S= Stop, T= TimingPoint (non stopping location) D = Destination
-    'TrainOrgin': 'TrainOrigin',
+    'LocationType': 'LocationType',
+    'TrainOrigin': 'TrainOrigin',
     'TrainDestination': 'TrainDestination',
     'ScheduledArrival': 'ScheduledArrival',
     'ScheduledDeparture': 'ScheduledDeparture',
-    'Arrival (actual)': 'ArrivalActual',
-    'Departure (actual)': 'DepartureActual',
-    'StopType': 'StopType',  # C= Current N = Next
+    'arrival_actual': 'arrival_actual',
+    'departure_actual': 'departure_actual',
+    'StopType': 'StopType',
+    'fetched_at': 'fetched_at'
 }
-
 
 ### Station Stop Data by Station Code (with timeframe) ###
 URL_STATION_DATA_BY_CODE_WITH_MINUTES = ("http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins")
 FIELD_MAP_STATION_DATA_BY_CODE_WITH_MINUTES = {
-    'server_time': 'ServerTime',
-    'train_code': 'TrainCode',
-    'station_name': 'StationFullName',
-    'station_code': 'StationCode',
-    'query_time': 'QueryTime',
-    'train_date': 'TrainDate',
-    'origin': 'Origin',
-    'destination': 'Destination',
-    'origin_time': 'OriginTime',
-    'destination_time': 'DestinationTime',
-    'status': 'Status',
-    'last_location': 'LastLocation',
-    'due_in': 'DueIn',
-    'late': 'Late',
-    'expected_arrival': 'ExpArrival',
-    'expected_departure': 'ExpDepart',
-    'scheduled_arrival': 'SchArrival',
-    'scheduled_departure': 'SchDepart',
-    'direction': 'Direction',
-    'train_type': 'TrainType',
-    'location_type': 'LocationType',  # O= Origin, S= Stop, D = Destination
+    'ServerTime': 'ServerTime',
+    'TrainCode': 'TrainCode',
+    'StationFullName': 'StationFullName',
+    'StationCode': 'StationCode',
+    'QueryTime': 'QueryTime',
+    'TrainDate': 'TrainDate',
+    'Origin': 'Origin',
+    'Destination': 'Destination',
+    'OriginTime': 'OriginTime',
+    'DestinationTime': 'DestinationTime',
+    'Status': 'Status',
+    'LastLocation': 'LastLocation',
+    'DueIn': 'DueIn',
+    'Late': 'Late',
+    'ExpArrival': 'ExpArrival',
+    'ExpDepart': 'ExpDepart',
+    'SchArrival': 'SchArrival',
+    'SchDepart': 'SchDepart',
+    'Direction': 'Direction',
+    'TrainType': 'TrainType',
+    'LocationType': 'LocationType'
 }
